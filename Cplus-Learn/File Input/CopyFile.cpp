0ff001a6d0f01eb3796cc3c78c306fc9ca171c91
frerr <wubingfx@163.com>
2020-05-18 14:22:32 +0800

    add cplusplus

---
+++ Cplus-Learn/File Input/CopyFile.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+void Copyfile(ifstream & inF,ofstream & outF,const string &str); //一般stream objects打开/关闭是改变的，所以不适用const限定
+
+int main(){
+    ifstream inFile;
+    inFile.open("Summary");
+    ofstream outFile;
+    string name = "SummaryCpy.txt";
+    Copyfile(inFile,outFile,name);
+    return 0;
+}
+
+void Copyfile(ifstream & inF,ofstream & outF,const string &str){
+    outF.open(str);
+    if(! outF.is_open()){ //是否成功打开
+        cout << "open file "<< str << " Erro" << endl;
+        exit(EXIT_FAILURE);
+    }
+    string str1;
+    while (inF.good()) {
+        getline(inF,str1);
+        outF << str1 << endl;
+    }
+    cout << "Copy successfully !!" << endl;
+}
