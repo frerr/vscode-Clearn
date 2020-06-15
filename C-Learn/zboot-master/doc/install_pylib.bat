8aee15ced8d9c9c3b8c8bca56c8ec1a6ccee342f
frerr <wubingfx@163.com>
2020-06-15 20:42:59 +0800

    Delet c boot

--- C-Learn/zboot-master/doc/install_pylib.bat
+++
@@ -1,4 +0,0 @@
-pip3 install serial
-pip3 install pyserial
-pip3 install bincopy
-pause
--- C-Learn/zboot-master/firmware_bootloader/zboot_misc.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ZBOOT_MISC_H
-#define _ZBOOT_MISC_H
-
-#include "port.h"
-
-#define _USE_EEPROM 1
-#define _USE_CLI 1
-
-#endif
